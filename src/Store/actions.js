34feb5fef665729308d7cd1ecd314b0491c1f966
Jeremy Cleland <jeremy@clelandco.com>
2023-04-07 09:53:46 -0500

    fix actions

--- src/Store/actions.js
+++ src/Store/actions.js
@@ -1,20 +1,8 @@
-export const addToCart = (product) => {
-  return {
-    type: "ADD TO CART",
-    payload: product,
-  };
-};
+import axios from "axios";
 
-export const removeFromCart = (product) => {
+export const selectCategory = (category) => {
   return {
-    type: "REMOVE FROM CART",
-    payload: product,
-  };
-};
-
-export const setCategory = (category) => {
-  return {
-    type: "SET CATEGORY",
+    type: "SELECT-CATEGORY",
     payload: category,
   };
 };
@@ -24,4 +12,62 @@ export const reset = () => {
     type: "RESET",
     payload: {},
   };
+};
+
+export const addItem = (product) => {
+  return {
+    type: "ADD",
+    payload: product,
+  };
+};
+
+export const removeItem = (product) => {
+  return {
+    type: "REMOVE",
+    payload: product,
+  };
+};
+
+export const setProducts = (data) => {
+  return {
+    type: "SET-PRODUCTS",
+    payload: data,
+  };
+};
+
+export const setCategories = (data) => {
+  return {
+    type: "SET-CATEGORIES",
+    payload: data,
+  };
+};
+
+export const updateProduct = (product) => {
+  return {
+    type: "UPDATE-PRODUCTS",
+    payload: product,
+  };
+};
+/* eslint-disable no-unused-vars */
+export const getProducts = () => async (dispatch, getState) => {
+  let response = await axios.get(
+    "https://api-js401.herokuapp.com/api/v1/products"
+  );
+  dispatch(setProducts(response.data.results));
+};
+/* eslint-disable no-unused-vars */
+export const getCategories = () => async (dispatch, getState) => {
+  let response = await axios.get(
+    "https://api-js401.herokuapp.com/api/v1/categories"
+  );
+  dispatch(setCategories(response.data.results));
+};
+/* eslint-disable no-unused-vars */
+export const updateInventory = (product) => async (dispatch, getState) => {
+  product.inStock--;
+  let response = await axios.put(
+    `https://api-js401.herokuapp.com/api/v1/products/${product._id}`,
+    product
+  );
+  dispatch(updateProduct(response.data));
 };
